97db079d5aaa6f6659918100c7cbf88bb3f0e39c
Bryan Alexis <librale19@gmail.com>
2021-09-30 16:18:19 -0500

    First Commit

---
+++ Semana1/colecciones.py
@@ -0,0 +1,15 @@
+#lista
+cursos_back = ['python', 'flask', 'django']
+print(cursos_back)
+
+for cursos in cursos_back:
+    print(cursos)
+
+cursos_back.append('Node.js')
+print(cursos_back)
+
+cursos_back.index('flask')
+print(cursos_back)
+
+cursos_back.remove
+print(cursos_back)
---
+++ Semana1/repetitivos.py
@@ -0,0 +1,5 @@
+num = int(input("Ingrese un Numero: "))
+sum = 0
+for i in range(3):
+    sum += num
+print(sum)
