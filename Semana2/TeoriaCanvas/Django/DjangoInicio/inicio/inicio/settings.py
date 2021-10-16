1c1463e07332f9a9cc493ceb5142e44484307817
Bryan Alexis <librale19@gmail.com>
2021-10-15 20:29:28 -0500

    MenuYa project created

--- Semana2/TeoriaCanvas/Django/DjangoInicio/inicio/inicio/settings.py
+++ Semana2/TeoriaCanvas/Django/DjangoInicio/inicio/inicio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-ffk*r)li^pdm-)yh56hs)1s(bug55omn2*u(+c(8!ec5jcam+@'
+SECRET_KEY = 'django-insecure-ffk*r)li^pdm-)yh56hs)1s(bug55omn2*u(+c(8!ec5jcam+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ Semana3/ClaseEnVivo/ProyectoRappi/menuya/delivery/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.db.models.deletion import RESTRICT
+from django.db.models.fields import CharField
+
+# Create your models here.
+
+
+class Negocio(models.Model):
+    nombre = models.CharField(max_length=200)
+    imagen = models.ImageField(upload_to='negocio', blank=True, null=True)
+    precioEnvio = models.DecimalField(max_digits=9, decimal_places=2)
+    calificacion = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nombre
+
+    ''' class Meta:
+        verbose_name_plural = "Negocio" '''
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
+    ''' class Meta:
+        verbose_name_plural = "Categoria" '''
+
+
+class Plato(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.RESTRICT)
+    nombre = CharField(max_length=200)
+    imagen = models.ImageField(upload_to='plato', blank=True, null=True)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=9, decimal_places=2)
+    negocio = models.ForeignKey(Negocio, on_delete=RESTRICT)
+
+    def __str__(self):
+        return self.nombre
+
+    ''' class Meta:
+        verbose_name_plural = "Plato" '''
